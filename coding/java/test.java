87f26cd9cbef78ed950d91f16a432918cc0785cc
wangpeiwen <2308498099@qq.com>
2025-09-11 23:18:52 +0800

    feat: 新增八股练习

--- coding/java/test.java
+++ coding/java/test.java
@@ -50,6 +50,26 @@ public class test {
         Arrays.sort(nums, i + 1, n);
     }
 
+    // sliding window top k
+    // public int[] maxSlidingWindow(int[] nums, int k) {
+    //     // 1. nums的长度为0, 或k为0
+    //     if (nums.length == 0 || k == 0) {
+    //         return new int[0];
+    //     }
+    //     // 2. nums的长度小于等于k
+    //     if (nums.length <= k) {
+    //         int max = Integer.MIN_VALUE;
+    //         for (int num : nums) {
+    //             max = Math.max(max, num);
+    //         }
+    //         return new int[]{max};
+    //     }
+    //     // i,j为滑动窗口的左右边界
+    //     int i=0,j=nums.length-k;
+    //     int[] res = new int[nums.length - k + 1];
+    //     for()
+    // }
+
     public static void main(String[] args) {
         test t = new test();
         // int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
